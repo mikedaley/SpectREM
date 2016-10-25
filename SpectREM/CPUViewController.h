602f34c805efb13de2bdbb6196833552044563b0
Mike Daley <mike@daley-uk.com>
2016-10-25 19:58:30 +0100

    Starting to add CPU view

---
+++ SpectREM/CPUViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPUViewController.h
+//  SpectREM
+//
+//  Created by Mike Daley on 24/10/2016.
+//  Copyright © 2016 71Squared Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CPUViewController : NSWindowController
+
+@end
---
+++ SpectREM/CPUViewController.m
@@ -0,0 +1,23 @@
+//
+//  CPUViewController.m
+//  SpectREM
+//
+//  Created by Mike Daley on 24/10/2016.
+//  Copyright © 2016 71Squared Ltd. All rights reserved.
+//
+
+#import "CPUViewController.h"
+
+@interface CPUViewController ()
+
+@end
+
+@implementation CPUViewController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
