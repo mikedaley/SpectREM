f3b63bfc4bd5b8980c7e7c4ce307ef9981d33a2b
Mike Daley <mike@71squared.com>
2016-11-08 09:47:17 +0000

    Added AY chip channel selectors to config view

---
+++ SpectREM/ConfigClipView.h
@@ -0,0 +1,13 @@
+//
+//  ConfigClipView.h
+//  SpectREM
+//
+//  Created by Mike Daley on 08/11/2016.
+//  Copyright © 2016 71Squared Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ConfigClipView : NSClipView
+
+@end
