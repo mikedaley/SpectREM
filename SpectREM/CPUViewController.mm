7e3543ba575a0c0b30dae3ce81527461dac6dea2
Mike Daley <mike@71squared.com>
2016-11-18 16:25:18 +0000

    Updated the prefix for hex numbers in the CPU view from 0x to $

--- SpectREM/CPUViewController.mm
+++ SpectREM/CPUViewController.mm
@@ -31,31 +31,31 @@
     
     if (!self.decimalFormat)
     {
-        self.pc = [NSString stringWithFormat:@"0x%04X", core->GetRegister(CZ80Core::eREG_PC)];
-        self.sp = [NSString stringWithFormat:@"0x%04X", core->GetRegister(CZ80Core::eREG_SP)];
-        self.a = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_A)];
-        self.f = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_F)];
-        self.b = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_B)];
-        self.c = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_C)];
-        self.d = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_D)];
-        self.e = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_E)];
-        self.h = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_H)];
-        self.l = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_L)];
-        self.i = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_I)];
-        self.r = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_R)];
-        self.im = [NSString stringWithFormat:@"0x%02X", core->GetIMMode()];
-        self.ix = [NSString stringWithFormat:@"0x%04X", core->GetRegister(CZ80Core::eREG_IX)];
-        self.iy = [NSString stringWithFormat:@"0x%04X", core->GetRegister(CZ80Core::eREG_IY)];
+        self.pc = [NSString stringWithFormat:@"$%04X", core->GetRegister(CZ80Core::eREG_PC)];
+        self.sp = [NSString stringWithFormat:@"$%04X", core->GetRegister(CZ80Core::eREG_SP)];
+        self.a = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_A)];
+        self.f = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_F)];
+        self.b = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_B)];
+        self.c = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_C)];
+        self.d = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_D)];
+        self.e = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_E)];
+        self.h = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_H)];
+        self.l = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_L)];
+        self.i = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_I)];
+        self.r = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_R)];
+        self.im = [NSString stringWithFormat:@"$%02X", core->GetIMMode()];
+        self.ix = [NSString stringWithFormat:@"$%04X", core->GetRegister(CZ80Core::eREG_IX)];
+        self.iy = [NSString stringWithFormat:@"$%04X", core->GetRegister(CZ80Core::eREG_IY)];
 
     
-        self.aa = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_A)];
-        self.ff = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_F)];
-        self.bb = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_B)];
-        self.cc = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_C)];
-        self.dd = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_D)];
-        self.ee = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_E)];
-        self.hh = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_H)];
-        self.ll = [NSString stringWithFormat:@"0x%02X", core->GetRegister(CZ80Core::eREG_ALT_L)];
+        self.aa = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_A)];
+        self.ff = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_F)];
+        self.bb = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_B)];
+        self.cc = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_C)];
+        self.dd = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_D)];
+        self.ee = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_E)];
+        self.hh = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_H)];
+        self.ll = [NSString stringWithFormat:@"$%02X", core->GetRegister(CZ80Core::eREG_ALT_L)];
     }
     else
     {
