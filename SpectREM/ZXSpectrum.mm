151b10b83e28dbbbb7b56a8d4462afab9724e893
Mike Daley <mike@71squared.com>
2017-01-18 19:42:13 +0000

    Updated floating bus table to use the machines top border value rather than hard coded values

--- SpectREM/ZXSpectrum.mm
+++ SpectREM/ZXSpectrum.mm
@@ -763,13 +763,13 @@ static unsigned char floatingBus(void *m)
     
     // If the line and tState are within the bitmap of the screen then grab the
     // pixel or attribute value
-    if (currentDisplayLine >= (56 + machine->machineInfo.pxVerticalBlank)
-        && currentDisplayLine < (56 + machine->machineInfo.pxVerticalBlank + machine->machineInfo.pxVerticalDisplay)
+    if (currentDisplayLine >= (machine->machineInfo.pxTopBorder + machine->machineInfo.pxVerticalBlank)
+        && currentDisplayLine < (machine->machineInfo.pxTopBorder + machine->machineInfo.pxVerticalBlank + machine->machineInfo.pxVerticalDisplay)
         && currentTs <= machine->machineInfo.tsHorizontalDisplay)
     {
         unsigned char ulaValueType = cFloatingBusTable[ currentTs & 0x07 ];
         
-        int y = currentDisplayLine - (56 + machine->machineInfo.pxVerticalBlank);
+        int y = currentDisplayLine - (machine->machineInfo.pxTopBorder + machine->machineInfo.pxVerticalBlank);
         int x = currentTs >> 2;
         
         if (ulaValueType == FloatingBusValueType::ePixel)
